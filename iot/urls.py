1c38251ebd2005528cff3957ca6345b52dd9cb40
mohy <30950062+mohy1376@users.noreply.github.com>
2018-08-12 11:23:08 +0430

    add signuppage to cms

--- iot/urls.py
+++ iot/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 from . import views
-
+app_name = "iot"
 urlpatterns = [
     url(r'^index', views.index, name='index'),
     url(r'^signup', views.UserFormView.as_view(), name='signup')
