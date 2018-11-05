cb6cb879a5c9542e4a437f80f1b48876df278375
mohy <30950062+mohy1376@users.noreply.github.com>
2018-11-05 23:37:55 +0330

    chage search jquery

--- mysite/static/js/karaco.js
+++ mysite/static/js/karaco.js
@@ -1,26 +1,26 @@
 $(document).ready(function() {
-
-
   $("#btnSearch").click(function() {
-    
-    
-      $("#search").fadeToggle(0, function(){
+ 
+    if($("#btnSearch").offset().top < 50){
 
-        if($('#search').is(":visible")){
-          
-          $('.header_logo').css("margin-top","0px");
+      $("#search").fadeToggle(0, function() {
+        if ($("#search").is(":visible")) {
+          $(".header_logo").css("margin-top", "0px");
+        } else {
+          $(".header_logo").css("margin-top", "34px");
         }
-        else{
-          
-          $('.header_logo').css("margin-top","34px");
-          
-        }
-
-
       });
+
+    }
+    else{
+      $("html, body").animate({ scrollTop: 0 }, "slow", );
+      $("#search").show(function(){
+        $(".header_logo").css("margin-top", "0px");
+
+
+      });  
+    }
 
 
   });
-
-
 });
